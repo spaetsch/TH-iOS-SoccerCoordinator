7a3630f4ae5a1ee2eefb3a7f25a4fabb1063d26d
Sarah Paetsch <spaetsch@gmail.com>
2016-04-15 16:17:24 -0700

    First commit. Project requirements

---
+++ SoccerCoordinator.playground/Contents.swift
@@ -0,0 +1,13 @@
+// Sarah Paetsch 4/15/16
+// Soccer League Coordinator Project
+// Treehouse iOS Techdegree
+
+
+
+// Manually create a single collection that contains all information for all 18 players. Each player should themselves be represented by their own collection.
+// Create appropriate variables and logic to sort and store players into three teams: Sharks, Dragons and Raptors. Be sure that your logic results in all teams having the same number of experienced players on each.
+// Provide logic that prints a personalized letter to the guardians specifying: the player’s name, guardians' names, team name, and date/time of their first team practice. The letters should be visible when code is placed in a XCode Playground or run in an XCode project.
+
+
+
+// array of dictionaries
